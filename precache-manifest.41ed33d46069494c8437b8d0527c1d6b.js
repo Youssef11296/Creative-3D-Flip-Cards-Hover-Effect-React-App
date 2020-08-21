3517d117dc855899c6aaaf738ceb69bdfd658f1e
Youssef Shaaban <youssef@gmail.com>
2020-08-21 17:02:32 +0200

    Updates

---
+++ precache-manifest.41ed33d46069494c8437b8d0527c1d6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f04a87e2e9db068d38eb9430b8b44c3",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/index.html"
+  },
+  {
+    "revision": "ed8d5fa103aebd9738d7",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/static/css/2.5a9a206f.chunk.css"
+  },
+  {
+    "revision": "f2963a04bcbeedad8146",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/static/css/main.e5321f61.chunk.css"
+  },
+  {
+    "revision": "ed8d5fa103aebd9738d7",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/static/js/2.7e06e353.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/static/js/2.7e06e353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2963a04bcbeedad8146",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/static/js/main.203277a8.chunk.js"
+  },
+  {
+    "revision": "4db3c05187c0f2cc26db",
+    "url": "/Creative-3D-Flip-Cards-Hover-Effect-React-App/static/js/runtime-main.7a33247d.js"
+  }
+]);
